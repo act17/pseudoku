22df0e2a24d292c7db5192b16477e448acdc21f2
Aiden Taylor <aiden.taylor@protonmail.com>
2023-01-19 11:18:22 -0700

    Added header information for the two files added in ./src/gui.

--- src/pseudoku.h
+++ src/pseudoku.h
@@ -4,4 +4,7 @@
 void pseudokugen(int Puzzle[9][9], int Seed);
 void pseudokudel(int Puzzle[9][9], int Seed, int Difficulty);
 
+void guiwrapper(int Puzzle[9][9], int Answer[9][9]);
+void guimainmenu(int MaxY, int MaxX);
+
 #endif
