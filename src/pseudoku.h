c8b664e8b6cf925433ff86386d151c0781f83ccc
Aiden Taylor <aiden.taylor@protonmail.com>
2023-01-21 19:13:54 -0700

    Added header for void guigame() and updated void guiwrapper().

--- src/pseudoku.h
+++ src/pseudoku.h
@@ -4,7 +4,8 @@
 void pseudokugen(int Puzzle[9][9], int Seed);
 void pseudokudel(int Puzzle[9][9], int Seed, int Difficulty);
 
-void guiwrapper(int Puzzle[9][9], int Answer[9][9]);
+void guiwrapper(int Puzzle[9][9], int Answer[9][9], int Seed);
 void guimainmenu(int MaxY, int MaxX);
+void guigame(int Puzzle[9][9], int Answer[9][9], int MaxY, int MaxX, int Seed);
 
 #endif
