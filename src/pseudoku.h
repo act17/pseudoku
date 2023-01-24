2defa43f4345d53905e215f144e375b533168acc
Aiden Taylor <aiden.taylor@protonmail.com>
2023-01-23 22:21:39 -0700

    Added a new function and defined two constant strings for version identification.

--- src/pseudoku.h
+++ src/pseudoku.h
@@ -1,8 +1,12 @@
 #ifndef __PSEUDOKU_H
 #define __PSEUDOKU_H
 
+static const char VERSION_NUM[7] = "B1.2.0";
+static const char VERSION_DATE[11] = "2023-01-21";
+
 void pseudokugen(int Puzzle[9][9], int Seed);
 void pseudokudel(int Puzzle[9][9], int Seed, int Difficulty);
+int pseudokucmp(int Puzzle[9][9], int Answer[9][9], int Wrong[9][9]);
 
 void guiwrapper(int Puzzle[9][9], int Answer[9][9], int Seed);
 void guimainmenu(int MaxY, int MaxX);
