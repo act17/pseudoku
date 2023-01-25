1ae3def0b3ab4f979d6e97ac80b407ce0f45e4e6
Aiden Taylor <aiden.taylor@protonmail.com>
2023-01-24 20:55:06 -0700

    Updated for a variety of reasons.

--- src/main.c
+++ src/main.c
@@ -1,19 +1,14 @@
 #include "pseudoku.h"
 #include <stdio.h>
-#include <time.h>
 
 int main()
 {
 
   int Puzzle[9][9];
   int Answer[9][9];
-  int Seed = time(0);
+  int Options[3];
 
-  pseudokugen(Puzzle, Seed);
-  pseudokugen(Answer, Seed);
-  pseudokudel(Puzzle, Seed, 20);
-
-  guiwrapper(Puzzle, Answer, Seed);
+  guiwrapper(Puzzle, Answer, Options);
 
 
   return 0;
