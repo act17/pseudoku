0a9819952e796c805f602cf8e586b0533921c294
Aiden Taylor <aiden.taylor@protonmail.com>
2023-01-26 18:33:12 -0700

    Added new headers and altered already existing ones.

--- src/pseudoku.h
+++ src/pseudoku.h
@@ -1,8 +1,8 @@
 #ifndef __PSEUDOKU_H
 #define __PSEUDOKU_H
 
-static const char VERSION_NUM[7] = "B1.3.0";
-static const char VERSION_DATE[11] = "2023-01-24";
+static const char VERSION_NUM[8] = "B1.4.0";
+static const char VERSION_DATE[11] = "2023-01-26";
 
 void pseudokugen(int Puzzle[9][9], int Seed);
 void pseudokudel(int Puzzle[9][9], int Seed, int Difficulty);
@@ -10,7 +10,8 @@ int pseudokucmp(int Puzzle[9][9], int Answer[9][9], int Wrong[9][9]);
 
 void guiwrapper(int Puzzle[9][9], int Answer[9][9], int Options[3]);
 void guimainmenu(int MaxY, int MaxX);
+void guiseed(int MaxY, int MaxX, int Options[3]);
 void guidifficulty(int MaxY, int MaxX, int Options[3]);
-void guigame(int Puzzle[9][9], int Answer[9][9], int MaxY, int MaxX, int Seed);
+void guigame(int Puzzle[9][9], int Answer[9][9], int MaxY, int MaxX, int Options[3]);
 
 #endif
