34edc945e601e25212f965dc90c77389df09fd2a
Aiden Taylor <aiden.taylor@protonmail.com>
2023-01-27 11:36:58 -0700

    Formatted with astyle.

--- src/pseudoku/pseudokudel.c
+++ src/pseudoku/pseudokudel.c
@@ -12,22 +12,22 @@ void pseudokudel(int Puzzle[9][9], int Seed, int Difficulty)
   // The following block of code will replace a random entry of the
   // puzzle array as many times as the integer, Difficulty, insists.
   for(int i = 0; i < Difficulty; i++)
+  {
+
+    // This loop will repeat until a non-zero entry is found.
+    while(Puzzle[Y][X] == 0)
     {
-
-      // This loop will repeat until a non-zero entry is found.
-      while(Puzzle[Y][X] == 0)
-        {
-          X = rand() % 9;
-          Y = rand() % 9;
-        }
-
-      // When found, the entry is set to zero, and a new random entry is set.
-      Puzzle[Y][X] = 0;
-
       X = rand() % 9;
       Y = rand() % 9;
-
     }
+
+    // When found, the entry is set to zero, and a new random entry is set.
+    Puzzle[Y][X] = 0;
+
+    X = rand() % 9;
+    Y = rand() % 9;
+
+  }
 
   return;
 }
